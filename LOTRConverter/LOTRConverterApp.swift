1978644149deedc0ee9eac421ecd8fdea0348ee1
ribragimov-mwb <ribragimov@malwarebytes.com>
2025-07-05 10:28:07 +0300

    Initial Commit

---
+++ LOTRConverter/LOTRConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  LOTRConverterApp.swift
+//  LOTRConverter
+//
+//  Created by Ramill Ibragimov on 7/5/25.
+//
+
+import SwiftUI
+
+@main
+struct LOTRConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
